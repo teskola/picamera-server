4f90db018b67360aaf18894c0288f2ef565a18c9
timo.eskola@tuni.fi <timo.eskola@tuni.fi>
2024-02-22 03:56:48 +0200

    simultaneous stream and capture

--- mjpeg_server_2.py
+++ mjpeg_server_2.py
@@ -10,10 +10,9 @@ from http import server
 from threading import Condition
 
 from client import client
-from camera import instance as camera
-from picamera2.encoders import MJPEGEncoder
+from picamera2 import Picamera2
+from picamera2.encoders import MJPEGEncoder, H264Encoder
 from picamera2.outputs import FileOutput
-
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -25,37 +24,17 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
+camera = Picamera2()
+
+# Main configuration for captured videos, lores configuration for stream
+
+video_config = camera.create_video_configuration(main={"size": (1280, 720)}, lores={"size": (640, 480)})
+still_config = camera.create_still_configuration()
+stream_output = StreamingOutput()
+video_encoder = H264Encoder()
+
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
-    
-    def capture_still(self):
-        bucket = "raspberry"
-
-        # Configure for high-quality still picture, take picture
-
-        camera_config = camera.create_still_configuration()
-        camera.switch_mode(camera_config)
-        data = io.BytesIO()
-        camera.capture_file(data, format='jpeg')
-        camera.switch_mode(camera.create_video_configuration(main={"size": (640, 480)}))
-
-        # Change the stream position to start for Minio to get correct bytes
-
-        data.seek(0)
-
-        # Upload to Minio
-
-        result = client().put_object(
-            bucket, "capture.jpg", data, len(data.getvalue()),
-            content_type="image/jpg")
-
-
-        print(
-            "created {0} object; etag: {1}".format(
-                result.object_name, result.etag,
-            ))
-
-
     
     def do_GET(self):
         if self.path == '/':
@@ -71,7 +50,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
-            self.capture_still()
+            capture_still()
         elif self.path == '/index.html':
             with open('index.html', 'r') as f:
                 html_string = f.read()
@@ -90,9 +69,9 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.end_headers()
             try:
                 while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
+                    with stream_output.condition:
+                        stream_output.condition.wait()
+                        frame = stream_output.frame
                     self.wfile.write(b'--FRAME\r\n')
                     self.send_header('Content-Type', 'image/jpeg')
                     self.send_header('Content-Length', len(frame))
@@ -109,17 +88,66 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
 
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    
+    
     allow_reuse_address = True
     daemon_threads = True
 
 
-camera.configure(camera.create_video_configuration(main={"size": (640, 480)}))
-output = StreamingOutput()
-camera.start_recording(MJPEGEncoder(), FileOutput(output))
+# def start_recording():
+    
 
-try:
-    address = ('', 8000)
-    server = StreamingServer(address, StreamingHandler)
-    server.serve_forever()
-finally:
-    camera.stop_recording()
+def capture_still():
+        bucket = "raspberry"
+
+        # Configure for high-quality still picture, take picture and switch back to streaming config
+
+        camera.switch_mode(still_config)
+        data = io.BytesIO()
+        camera.capture_file(data, format='jpeg')
+        camera.switch_mode(video_config)
+
+        # Change the stream position to start for Minio to get correct bytes
+
+        data.seek(0)
+        
+        # Upload to Minio
+
+        result = client().put_object(
+            bucket, "capture.jpg", data, len(data.getvalue()),
+            content_type="image/jpg")
+
+
+        print(
+            "created {0} object; etag: {1}".format(
+                result.object_name, result.etag,
+            ))
+
+def main():  
+    
+    camera.configure(video_config)
+    mjpeg_encoder = MJPEGEncoder()
+    mjpeg_encoder.framerate = 30
+    mjpeg_encoder.size = video_config["lores"]["size"]
+    mjpeg_encoder.format = video_config["lores"]["format"]
+    mjpeg_encoder.bitrate = 5000000    
+    mjpeg_encoder.output = FileOutput(stream_output)
+    video_encoder.output = FileOutput('test.h264')
+    camera.start_encoder(mjpeg_encoder)
+    camera.start_encoder(video_encoder)
+    camera.start() 
+    
+
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+        
+    finally:    
+        mjpeg_encoder.stop()
+        video_encoder.stop()
+        camera.stop()
+        
+
+if __name__ == "__main__":
+    main()
