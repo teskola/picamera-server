d0b55166fc3678d98673e20719a4cbd67b491034
timo.eskola@tuni.fi <timo.eskola@tuni.fi>
2024-02-23 21:28:56 +0200

    camera start/stop

--- mjpeg_server_2.py
+++ mjpeg_server_2.py
@@ -26,7 +26,7 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
-
+# open camera
 
 camera_running = False
 camera = Picamera2()
@@ -47,7 +47,8 @@ video_config = camera.create_video_configuration(
 still_config = camera.create_still_configuration(
     lores={"size": STREAM_RESOLUTION})
 
-camera.configure(video_config)
+logging.info("Configure to still.")
+camera.configure(still_config)
 
 # minio
 
@@ -56,6 +57,9 @@ bucket = "raspberry"
 # encoders
 
 stream_encoder = MJPEGEncoder()
+
+def encoders_running():
+    return len(camera.encoders) > 0
 
 
 def start_camera():
@@ -66,10 +70,11 @@ def start_camera():
 
 def stop_camera():
     global camera_running
-    if (len(camera.encoders) < 1):
+    if not encoders_running():        
         camera.stop()
+        logging.info("Configure to still.")
+        camera.configure(still_config)
         camera_running = False
-        logging.info("Camera stopped.")
 
 def capture_still():
         
@@ -77,36 +82,32 @@ def capture_still():
     
     start_camera()
     
-    # If recording, pause recording.
+    # If recording, pause recording. Configure for still image.
     
     video_capture_paused = False
-    
-    if video_capture.encoder in camera.encoders:
-        camera.stop_encoder(encoders=[video_capture.encoder])
-        video_capture_paused = True
-        logging.info("Recording paused.")
-    
-
-    # Configure for high-quality still picture, 
-    # take picture and switch back to streaming config
-    
-    camera.switch_mode(still_config)
-    logging.info("Configurated for high quality still image")
+            
+    if encoders_running():        
+        if video_capture.encoder in camera.encoders:
+            camera.stop_encoder(encoders=[video_capture.encoder])
+            video_capture_paused = True
+            logging.info("Recording paused.")
+        logging.info("Configure to still.")
+        camera.switch_mode(still_config)
     
     data = io.BytesIO()
     camera.capture_file(data, format='jpeg')
     logging.info("Image captured")
-    camera.switch_mode(video_config)
-    logging.info("Configuration changed: format: %s, resolution: %s",
-        video_capture.encoder.format, video_capture.encoder.size)
-    
-    
+        
     # Resume paused recording
     
-    if video_capture_paused:
-        video_capture.start()
-    
-    stop_camera()
+    if encoders_running():
+        logging.info("Configure to video.")
+        camera.switch_mode(video_config)
+        if video_capture_paused:
+            video_capture.start()
+    else:
+        camera.stop()
+        camera_running = False   
 
     # Change the stream position to start for Minio to get correct bytes
 
@@ -161,37 +162,19 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_response(301)
             self.send_header('Location', '/index.html')
             self.end_headers()
-        elif self.path == '/video_start':
-            with open('index.html', 'r') as f:
-                html_string = f.read()
-            content = html_string.encode('utf-8')
+        elif self.path == '/video_start':            
             self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
             self.end_headers()
-            self.wfile.write(content)
             video_capture.start()
-        elif self.path == '/video_stop':
-            with open('index.html', 'r') as f:
-                html_string = f.read()
-            content = html_string.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
+        elif self.path == '/video_stop':            
+            self.send_response(200)            
             self.end_headers()
-            self.wfile.write(content)
             video_capture.stop()
             video_capture.upload('test')
-        elif self.path == '/still':
-            with open('index.html', 'r') as f:
-                html_string = f.read()
-            content = html_string.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
+        elif self.path == '/still':  
+            capture_still()          
+            self.send_response(200)            
             self.end_headers()
-            self.wfile.write(content)
-            capture_still()
         elif self.path == '/index.html':
             with open('index.html', 'r') as f:
                 html_string = f.read()
@@ -202,24 +185,27 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(content)
         elif self.path == '/stream.mjpg':
+            
+            if not encoders_running():
+                logging.info("Configure to video.")
+                camera.configure(video_config)
+                camera.start_encoder(
+                    encoder=stream_encoder, 
+                    quality=Quality.LOW, 
+                    output=FileOutput(stream_output), 
+                    name="lores")
+                start_camera()
+                logging.info("Stream started.")
+
+            stream_clients.add(self.client_address)            
             self.send_response(200)
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
             self.send_header('Pragma', 'no-cache')
             self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
             self.end_headers()
-            try:
-                if (len(stream_clients) < 1):
-                    camera.start_encoder(
-                        encoder=stream_encoder, 
-                        quality=Quality.LOW, 
-                        output=FileOutput(stream_output), 
-                        name="lores")
-                    start_camera()
-                    logging.info("Streaming started.")
-                    
-                stream_clients.add(self.client_address)
-                                
+            
+            try:               
                 while True:
                     with stream_output.condition:
                         stream_output.condition.wait()
@@ -232,12 +218,14 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     self.wfile.write(b'\r\n')
             except Exception as e:
                 stream_clients.remove(self.client_address)
-                if (len(stream_clients) < 1):
-                    camera.stop_encoder()
-                    stop_camera()
                 logging.info(
                     'Removed streaming client %s: %s',
                     self.client_address, str(e))
+                if (len(stream_clients) < 1):
+                    camera.stop_encoder(encoders=[stream_encoder])
+                    logging.info("Stream stopped.")                    
+                    stop_camera()
+                
         else:
             self.send_error(404)
             self.end_headers()
