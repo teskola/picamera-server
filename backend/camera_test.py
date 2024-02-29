205486151ac20dd5bf93b8d39729095b2ef5365c
timo <timo.eskola@tuni.fi>
2024-02-29 12:26:52 +0200

    tests

--- backend/camera_test.py
+++ backend/camera_test.py
@@ -27,14 +27,14 @@ class FrameRateTests(unittest.TestCase):
         self.picam2 = Picamera2()
         self.picam2.configure(self.picam2.create_still_configuration())
         self.picam2.start()
-        interval = 0.5
+        interval = 0.2
 
-        for i in range(int(1/interval)):
+        for i in range(50):
             Thread(target=self._capture_jpg).start()
             sleep(interval) 
         sleep(interval)                     
         self.picam2.close()
-        self.assertEqual(self.captured_images, int(1/interval))
+        self.assertEqual(self.captured_images, 50)
         self.captured_images = 0
     
     def test_framerate_30(self):
