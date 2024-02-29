2160f65dbd62601ee0db0b46af76449bb2a35453
timo <timo.eskola@tuni.fi>
2024-02-29 10:20:30 +0200

    tests

--- backend/camera_test.py
+++ backend/camera_test.py
@@ -19,7 +19,7 @@ class FrameRateTests(unittest.TestCase):
         t1 = threading.Thread(target=camera.preview_start)
         t1.start()
         time.sleep(10)
-        self.camera.preview_stop()
+        camera.preview_stop()
         t1.join()
         print(f"{self.framecount} frames in 10 seconds")
         self.assertTrue(self.framecount > 275)
@@ -31,12 +31,7 @@ class FrameRateTests(unittest.TestCase):
     def test_framerate_50(self):
         camera = Camera()
         camera.picam2.pre_callback = self._increase_frame_count
-
-        t1 = threading.Thread(target=camera.capture_timelapse(interval=20))
-        t1.start()
-        time.sleep(10)
-        self.camera.preview_stop()
-        t1.join()
+        camera.capture_timelapse(interval=20)        
         print(f"{self.framecount} frames in 10 seconds")
         self.assertTrue(self.framecount > 400)
         self.assertTrue(self.framecount < 500)
