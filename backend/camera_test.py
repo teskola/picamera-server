cabe4fc71a1aada81c256771dd9d77df486d5a33
timo <timo.eskola@tuni.fi>
2024-02-29 15:19:27 +0200

    tests

--- backend/camera_test.py
+++ backend/camera_test.py
@@ -34,21 +34,5 @@ class FrameRateTests(unittest.TestCase):
             self.assertGreater(len(image.getvalue()), 0)        
         self.camera.picam2.close()
     
- """    def test_framerate_30(self):
-        self.camera = Camera()
-        self.camera.picam2.pre_callback = self._increase_frame_count        
-        t1 = Thread(target=self.camera.preview_start)
-        t1.start()
-        sleep(10)
-        self.camera.preview_stop()
-        self.camera.picam2.close()
-        t1.join()
-        print(f"{self.framecount} frames in 10 seconds")
-        self.assertGreater(self.framecount, 275)
-        self.assertLess(self.framecount, 300)
-        self.framecount = 0
- """
-    
-
 if __name__ == '__main__':
     unittest.main()
