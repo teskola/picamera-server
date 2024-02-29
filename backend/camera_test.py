7a4823a183dfc55b75c13f8872ac542697107512
timo <timo.eskola@tuni.fi>
2024-02-29 10:40:01 +0200

    tests

--- backend/camera_test.py
+++ backend/camera_test.py
@@ -2,6 +2,9 @@ import unittest
 import threading
 import time
 from camera import Camera
+from picamera2 import Picamera2
+from libcamera import controls
+
 
 class FrameRateTests(unittest.TestCase):
 
@@ -10,12 +13,30 @@ class FrameRateTests(unittest.TestCase):
         super().__init__(methodName)    
 
     def _increase_frame_count(self, request):        
-        self.framecount += 1
-
+        self.framecount += 1   
+        
+    def test_framerate_50(self):
+        picam2 = Picamera2()
+        picam2.pre_callback = self._increase_frame_count
+        config = picam2.create_preview_configuration(raw=picam2.sensor_modes[0])
+        picam2.set_controls({"NoiseReductionMode": controls.draft.NoiseReductionModeEnum.Fast, 
+                                  "AeEnable": False, 
+                                  "AwbEnable": False, 
+                                  "FrameDurationLimits": (20000, 20000)})
+        picam2.configure(config)
+        picam2.start()
+        time.sleep(10)
+        picam2.stop()
+        print(f"{self.framecount} frames in 10 seconds")
+        self.assertTrue(self.framecount > 400)
+        self.assertTrue(self.framecount < 500)
+        self.framecount = 0
+        picam2.close()
+    
     def test_framerate_30(self):
         camera = Camera()
         camera.picam2.pre_callback = self._increase_frame_count
-
+        
         t1 = threading.Thread(target=camera.preview_start)
         t1.start()
         time.sleep(10)
@@ -25,16 +46,6 @@ class FrameRateTests(unittest.TestCase):
         print(f"{self.framecount} frames in 10 seconds")
         self.assertTrue(self.framecount > 275)
         self.assertTrue(self.framecount < 300)
-        self.framecount = 0
-        
-    
-    def test_framerate_50(self):
-        camera = Camera()
-        camera.picam2.pre_callback = self._increase_frame_count
-        camera.capture_timelapse(interval=20)        
-        print(f"{self.framecount} frames in 10 seconds")
-        self.assertTrue(self.framecount > 400)
-        self.assertTrue(self.framecount < 500)
         self.framecount = 0
 
     
