d696176be81db29e5ed06f2ff627540ac76716d5
timo <timo.eskola@tuni.fi>
2024-03-05 15:08:54 +0200

    capture timer

--- backend/server.py
+++ backend/server.py
@@ -77,7 +77,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                 self.send_response(409)
             self.end_headers()           
         elif self.path == '/still':
-            set_capture_timer(60, "testi", 0, False)
+            set_capture_timer(10, "testi", 0, True)
             self.send_response(200)
             self.end_headers()
         
