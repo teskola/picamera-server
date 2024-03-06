f5c2fef94e846b152521cb41d6125080c75360fa
timo <timo.eskola@tuni.fi>
2024-03-06 10:37:09 +0200

    status test

--- backend/camera.py
+++ backend/camera.py
@@ -107,7 +107,8 @@ class Camera:
                     main={"size": Resolutions.HALF},
                     lores={"size": Resolutions.STREAM_4_3},
                     controls={"FrameDurationLimits": (33333, 33333)},
-                    buffer_count = 6
+                    buffer_count = 6,
+                    use_case="default"
                 ),
                 'full':
                    self.picam2.create_still_configuration(
