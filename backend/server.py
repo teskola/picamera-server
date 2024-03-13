288ce30f05a08302b795b06ce29b419e7bcb936f
timo <timo.eskola@tuni.fi>
2024-03-13 12:05:25 +0200

    api test

--- backend/server.py
+++ backend/server.py
@@ -25,7 +25,6 @@ class CameraHandler(server.BaseHTTPRequestHandler):
         self.send_response(200, "ok")
         self.send_header('Access-Control-Allow-Origin', 'http://localhost:5173')
         self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST')
-        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
    
