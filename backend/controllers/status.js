16d5237ccbc71224a8e00ee83c5aae72eca68824
timo <timo.eskola@tuni.fi>
2024-03-14 14:06:24 +0200

    nodejs server

--- backend/controllers/status.js
+++ backend/controllers/status.js
@@ -1,22 +1,24 @@
 const socket = require('../socket')
 
 const getStatus = async (req, res) => {
-    try {
-        const response = socket.write('status');
-        if (response.length === 1) {
-          return res.send(response[0]);
-        }
-      } catch (err) {
-        console.log(err);
-        const response = {
-          OK: false,
-          statusCode: 500,
-          error: err,
-        };
-        return res.status(500).send(response);
-      }
+  try {
+    const response = socket.write('status');
+    socket.on('data', (stream) => {
+      console.log(stream)
+    })
+    //return res.send(response);
+
+  } catch (err) {
+    console.log(err);
+    const response = {
+      OK: false,
+      statusCode: 500,
+      error: err,
+    };
+    return res.status(500).send(response);
+  }
 }
 
 module.exports = {
-    getStatus
-  }
+  getStatus
+}
