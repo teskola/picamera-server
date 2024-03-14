ac8169e7906a2d02d373a719f84d3f5c6109b175
timo <timo.eskola@tuni.fi>
2024-03-14 16:17:21 +0200

    nodejs server

--- backend/controllers/status.js
+++ backend/controllers/status.js
@@ -1,31 +1,17 @@
 const socket = require('../socket')
 
 const getStatus = (req, res) => {
-  try {
-    const request = socket.write(JSON.stringify({action: 'status'}));
-    if (request) {
-      socket.once('data', (stream) => {
-        return res.send(stream.toString())
-      })
-    }
-    else {
-      const response = {
-        OK: false,
-        statusCode: 500,
-        error: err,
-      };
-      return res.status(500).send(response);
-    }    
-
-  } catch (err) {
-    console.log(err);
-    const response = {
-      OK: false,
-      statusCode: 500,
-      error: err,
-    };
-    return res.status(500).send(response);
+  const request = socket.write(JSON.stringify({ action: 'status' }));
+  if (request) {
+    socket.once('data', (stream) => {
+      return res.send(stream.toString())
+    })
   }
+  else {
+    console.log("Writing status to socket failed")
+    return res.status(500).send({ error: 'Something went wrong!' });
+  }
+
 }
 
 module.exports = {
