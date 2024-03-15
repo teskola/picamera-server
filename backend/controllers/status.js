680849b6a67fd90640f49fab22ad0e0a142c27f7
timo <timo.eskola@tuni.fi>
2024-03-15 02:52:36 +0200

    socket test

--- backend/controllers/status.js
+++ backend/controllers/status.js
@@ -2,8 +2,13 @@ const status = require('../models/status')
 
 const getStatus = async (req, res) => {
   try {
-    const response = await status.fetch()    
-    return res.send(response)
+    const response = await status.fetch()
+    if (response) {
+      return res.send(response)
+    } else {
+      return res.status(500).send({ error: 'Something went wrong!' });
+    }
+
   }
   catch (err) {
     console.log(err)
