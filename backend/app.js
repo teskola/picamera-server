3df1942fce5562ebfb66443f13bebc02b7d6a5b5
timo <timo.eskola@tuni.fi>
2024-03-16 22:13:02 +0200

    stream

--- backend/app.js
+++ backend/app.js
@@ -57,12 +57,12 @@ app.get("/stream", (req, res) => {
         'Content-Type': 'multipart/x-mixed-replace; boundary=FRAME'
     })
     const listener = (frame) => {
-        res.write(Buffer.from('--FRAME\r\n'))
+        res.write(Buffer.from('--FRAME\r\n', 'binary'))
         res.write('Content-Type: image/jpeg\r\n')
         res.write('Content-Length: ' + Object.keys(frame).length)
         res.write("\r\n")
         res.write(Buffer.from(frame, 'binary'))
-        res.write(Buffer.from("\r\n"))
+        res.write(Buffer.from("\r\n", 'binary'))
     }
     try {
         connection.on('data', listener)
