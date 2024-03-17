f9fa9847d3283e8b6f0db6efe53d0f12dad79832
timo <timo.eskola@tuni.fi>
2024-03-17 12:03:33 +0200

    preview refactor

--- backend/camera.py
+++ backend/camera.py
@@ -11,7 +11,7 @@ from picamera2 import Picamera2, Metadata
 from picamera2.encoders import MJPEGEncoder, H264Encoder, Quality
 from picamera2.outputs import FileOutput
 
-STREAM_BITRATE = 1048576
+STREAM_BITRATE = 1081344
 KEEP_ALIVE_LIMIT = 60
 scheduler = sched.scheduler(time.time, time.sleep)
 
