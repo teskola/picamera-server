89fcd68a39fe5e8e0eef254b56284e7fefae5dae
timo <timo.eskola@tuni.fi>
2024-03-17 07:23:40 +0200

    stream

--- backend/routes/preview.js
+++ backend/routes/preview.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { previewStart, previewStop } = require('../controllers/preview');
+const { previewStart, previewStop, addListener } = require('../controllers/preview');
 
-router.get('/live.mjpeg', getStatus)
+router.get('/live.mjpeg', addListener)
 router.patch('/start', previewStart)
 router.patch('/stop', previewStop)
 
