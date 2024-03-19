4e0ae733d2b8bbeb9ef629f252f909e22b5e1c94
timo <timo.eskola@tuni.fi>
2024-03-19 02:44:21 +0200

    test

--- backend/stream.js
+++ backend/stream.js
@@ -8,7 +8,7 @@ class Stream {
 
     #createConnection = () => {
         const conn = socket.connect()
-        const req = conn.write(JSON.stringify({ action: 'preview_listen' }), (err) => {
+        const req = conn.write(JSON.stringify({ action: 'preview_listen' } + '\n'), (err) => {
             if (err) {
                 console.log(err)
                 throw new Error(err)
