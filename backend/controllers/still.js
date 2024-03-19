7fe2dc814da5d1e4361e8d11f5800972f47ff405
timo <timo.eskola@tuni.fi>
2024-03-19 05:26:40 +0200

    error handling

--- backend/controllers/still.js
+++ backend/controllers/still.js
@@ -32,6 +32,7 @@ const stillStart = async (req, res) => {
 
     try {
         const response = await still.start(params)
+        console.log(response)
         if (response) {
             if (response.error) {
                 return res.status(409).send(response)
