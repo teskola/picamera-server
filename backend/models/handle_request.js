27bd93bbef12b108274d0b7560ef87ee01f43f61
timo <timo.eskola@tuni.fi>
2024-03-22 09:35:40 +0200

    req,res handlers

--- backend/models/handle_request.js
+++ backend/models/handle_request.js
@@ -2,7 +2,7 @@ const socket = require('../socket')
 
 const handleRequest = ({action, params = {}}) => new Promise((resolve, reject) => {
     const conn = socket.connect()
-    const req = conn.write(JSON.stringify({...action, ...params}))
+    const req = conn.write(JSON.stringify({action, ...params}))
     if (req) {
         conn.once('data', (stream) => resolve(JSON.parse(stream.toString())))
     } else {
