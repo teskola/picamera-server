875578b5788da8c617a3ddf8f26e44b11413d909
timo <timo.eskola@tuni.fi>
2024-03-26 14:44:24 +0200

    socket.io

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,13 @@
 const express = require("express")
 const cors = require("cors")
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+
 
 const status = require('./routes/status')
 const still = require('./routes/still')
 const video = require('./routes/video')
 const preview_route = require("./routes/preview")
 const status_listener = require('./status_listener')
-
-const PORT = 5000;
 
 app.use(
     cors({
@@ -26,6 +23,4 @@ app.get("/health", (req, res) => {
     res.send("OK")
 });
 
-server.listen(PORT, () => {
-    console.log(`Backend is running on port ${PORT}`);
-  });
+module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const app = require('./app');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
 const PORT = 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Backend is running on port ${PORT}`);
 });
+
+module.exports = io
