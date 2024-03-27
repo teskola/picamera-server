f4045f8a2f304fde3c226de37df62703ce88b9fb
timo <timo.eskola@tuni.fi>
2024-03-27 10:52:26 +0200

    delete video

--- backend/models/video.js
+++ backend/models/video.js
@@ -3,7 +3,7 @@ const { handleRequest } = require('./handle_request')
 
 const video = {
     upload: (params) => handleRequest({ action: 'video_upload', params: params }),
-    delete: (id) => handleRequest({ action: 'video_delete', params: { id: id } }),
+    delete: (params) => handleRequest({ action: 'video_delete', params: params }),
     stop: () => handleRequest({ action: 'video_stop' }),
     start: (params) => handleRequest({ action: 'video_start', params: params })
 }
