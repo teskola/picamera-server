c00afac4789def6f7199e6731c5f2db6198da239
timo <timo.eskola@tuni.fi>
2024-03-28 09:17:40 +0200

    register user

--- backend/register.js
+++ backend/register.js
@@ -2,47 +2,42 @@ const pool = require("./db/pool");
 const bcrypt = require('bcryptjs');
 const Joi = require('joi');
 
-const result = await main()
+const args = process.argv.slice(2)
 
-async function main () {
-    const args = process.argv.slice(2)
+const body = { email: args[0], password: args[1] }
 
-    const body = { email: args[0], password: args[1] }
+const schema = Joi.object({
+    email: Joi.string().email().max(255).required(),
+    password: Joi.string().min(6).max(72).required(),
+});
 
-    const schema = Joi.object({
-        email: Joi.string().email().max(255).required(),
-        password: Joi.string().min(6).max(72).required(),
-    });
+const { error } = schema.validate(body)
 
-    const { error } = schema.validate(body)
-
-    if (error) {        
-        return error
-    }
-
-    bcrypt.hash(body.password, 12, (err, hash) => {
-        if (err) {
-            return err
-        }
-        const newUser = { email: body.email, password: hash }
-        pool.getConnection((err, connection) => {
-            if (err) {
-                return err
-            }
-            connection.query('INSERT INTO users SET ?;', newUser, (err, result) => {
-                if (err) {
-                    return err
-                } else {
-                    return result
-                }
-            })
-            connection.release()
-        })
-    })
+if (error) {
+    console.log(error)
+    return
 }
+bcrypt.hash(body.password, 12, (err, hash) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const newUser = { email: body.email, password: hash }
 
-console.log(result)
-
-
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        connection.query('INSERT INTO users SET ?;', newUser, (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(result)
+            }
+        })
+        connection.release()
+    })
+})
 
 
